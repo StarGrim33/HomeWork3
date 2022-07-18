db182bc972fd5e1c909b803d2adb4e34d5bdd781
StarGrim33 <prosky33@gmail.com>
2022-07-18 22:13:51 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace JuniorHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int elements = 30;
+            int[] numbers = new int[elements];
+            Random random = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)  // Вывод массива
+            {
+                numbers[i] = random.Next(elements);
+                Console.Write(numbers[i] + ",");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i - 1] > numbers[i] && numbers[i + 1] < numbers[i])
+                {
+                    Console.WriteLine("\nЛокальный максимум" + numbers[i]);
+                }
+            }
+        }
+    }
+}
+
