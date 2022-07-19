d7ed2f5f4a65dd1da4404bdea6fa3335caf0e097
StarGrim33 <prosky33@gmail.com>
2022-07-19 21:41:26 +0300

    2

--- Program.cs
+++ Program.cs
@@ -15,14 +15,29 @@ namespace JuniorHomeWork
                 Console.Write(numbers[i] + ",");
             }
 
-            for (int i = 0; i < numbers.Length; i++)
+            for (int i = 0; i < 1; i++)
             {
-                if (numbers[i - 1] > numbers[i] && numbers[i + 1] < numbers[i])
+
+                if (numbers[0] > numbers[1])
                 {
-                    Console.WriteLine("\nЛокальный максимум" + numbers[i]);
+                    Console.WriteLine("\nКрайний максимум" + numbers[0]);
+                }
+                else
+                {
+                    break;
                 }
             }
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i-1] < numbers[i] && numbers[i] > numbers[i+1])
+                {
+                    Console.WriteLine("\nКрайний максимум" + numbers[i]);
+                }
+                
+            }
         }
+
     }
 }
 
